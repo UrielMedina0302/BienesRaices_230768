126e8cf34cb2a2448858fa1ff1712dce6d915069
Uriel-Abdallah-Medina-Torres <230768@utxicotepec.edu.mx>
2024-10-21 11:58:04 -0600

    Practica09
    agregación del paquete json, lock.json

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require(`express`);//Importa la librería para crear un servidor web
+
+//Instanciar nuestra aplicación web
+const app =express();
+
+//Configuramos nuestro servidor web
+const port =3000;
+app.listen(port,()=>{
+    console.log(`La aplicación ha iniciado en el puerto: ${port}`);
+})
+
+//Probamos las rutas para poder presentar mensajes al usuario a travez del navegador
+app.get("/", function(req, res){
+    res.send("Hola mundo atravez del navegador")
+})
+
+app.get("/QuienSoy",function(req, res){
+    res.json({"estudiante": "Uriel Abdallah Medina Torres",
+    "carrera":"TI DSM",
+    "grado": "4°",
+    "grupo": "B",
+     "asignatura":"Aplicación Orientada a Objetos (AWOS)"   
+    });
+    
+})
